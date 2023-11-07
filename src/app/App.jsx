3bdf417d6804e94eef97bfb9c493a92a4d76661a
Naiem1 <naiem4288@gmail.com>
2023-11-07 21:23:42 +0600

    initialize project setup

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import TaskPage from '../page/TaskPage';
+
+function App() {
+  return (
+    <div className="grid place-items-center bg-slate-300 w-full h-screen border-2">
+      <TaskPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/TaskPage.jsx
@@ -0,0 +1,8 @@
+
+const TaskPage = () => {
+  return (
+    <div className="w-[90%] m-auto bg-white h-[95%] rounded-lg">TaskPage</div>
+  )
+}
+
+export default TaskPage
